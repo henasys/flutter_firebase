42d690edb4b7bf506f5e5281c9de82751ca53799
Alex Hojong Lee <alexhojonglee@gmail.com>
2022-09-26 22:51:29 +0900

    add user.email display

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,7 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
@@ -9,20 +12,27 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<User?>(context);
+    final name = user != null? user.email : '';
     return Scaffold(
       appBar: AppBar(actions: [
         IconButton(
-            icon: Icon(Icons.logout),
+            icon: const Icon(Icons.logout),
             onPressed: () {
               FirebaseAuth.instance.signOut();
               //navigate
             }),
       ]),
       body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
         // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        // crossAxisAlignment: CrossAxisAlignment.center,
-        children: const [Text('Home')],
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text("Home", style: Theme.of(context).textTheme.headline5,),
+          const SizedBox(height: 20,),
+          Text("$name is logged in", style: Theme.of(context).textTheme.headline6,)
+        ],
       )),
     );
   }
