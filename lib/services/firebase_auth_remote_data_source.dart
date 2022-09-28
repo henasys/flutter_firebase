544cbebf8a63a6ec3cf66d73b4ad377bfc5b7095
Alex Hojong Lee <alexhojonglee@gmail.com>
2022-09-29 00:26:34 +0900

    use CREATE_CUSTOM_TOKEN_URL with FlutterConfig

--- lib/services/firebase_auth_remote_data_source.dart
+++ lib/services/firebase_auth_remote_data_source.dart
@@ -1,7 +1,8 @@
+import 'package:flutter_config/flutter_config.dart';
 import 'package:http/http.dart' as http;
 
 class FirebaseAuthRemoteDataSource {
-  final String url = '서버주소';
+  final String url = FlutterConfig.get('CREATE_CUSTOM_TOKEN_URL');
 
   Future<String> createCustomToken(Map<String, dynamic> user) async {
     final customTokenResponse = await http.post(Uri.parse(url), body: user);
